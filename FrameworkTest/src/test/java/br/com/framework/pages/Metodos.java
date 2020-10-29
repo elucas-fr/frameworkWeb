a05857b2c9592fb80605da286522c58d342e6ff4
Eliane Lucas <elucas@frame.com.br>
2020-10-28 21:50:02 -0300

    Inclusão navegador

--- FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
+++ FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
@@ -1,15 +1,44 @@
 package br.com.framework.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Metodos {
 
 
 WebDriver driver;
 
-public void navegador(String url, String navegador) {
+public void navegador(String site, String navegador) {
 	
+	if(navegador == "CHROME" || navegador == "FIREFOX") {
+		switch (navegador) {
+		
+		case "CHORME":
+		System.setProperty("webdriver.chorme.driver", "./Driver/chromedriver.exe");		
+		driver = new ChromeDriver();
+		driver.get(site);
+		driver.manage().window().maximize();
+		break;
+		
+		case "FIREFOX":
+			System.setProperty("webdriver.chorme.driver", "./Driver/chromedriver.exe");		
+			driver = new FirefoxDriver();
+			driver.get(site);
+			driver.manage().window().maximize();
+			break;
+			
+		
+		}
+		
 	
+		
+	} else {
+	System.out.println("Navegador não suprimido, digite CHROME e FIREFOX ");
+		
+			
+		
+	}
 	
 }
 
