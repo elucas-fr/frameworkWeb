00a543ba0e564227527d95b18027801b57a51e19
Eliane Lucas <elucas@frame.com.br>
2020-11-02 18:34:21 -0300

    Novos metodos

--- FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
+++ FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
@@ -1,58 +1,57 @@
 package br.com.framework.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Metodos {
 
-	
-	//Abrir nevegador
+	// Abrir nevegador
 
-WebDriver driver;
+	WebDriver driver;
 
-public void navegador(String site, String navegador) {
-	
-	if(navegador == "CHROME" || navegador == "FIREFOX") {
-		switch (navegador) {
-		
-		case "CHORME":
-		System.setProperty("webdriver.chorme.driver", "./Driver/chromedriver.exe");		
-		driver = new ChromeDriver();
-		driver.get(site);
-		driver.manage().window().maximize();
-		break;
-		
-		case "FIREFOX":
-			System.setProperty("webdriver.chorme.driver", "./Driver/chromedriver.exe");		
-			driver = new FirefoxDriver();
-			driver.get(site);
-			driver.manage().window().maximize();
-			break;
-			
-		
+	public void navegador(String site, String navegador) {
+
+		if (navegador == "CHROME" || navegador == "FIREFOX") {
+			switch (navegador) {
+
+			case "CHORME":
+				System.setProperty("webdriver.chorme.driver", "./Driver/chromedriver.exe");
+				driver = new ChromeDriver();
+				driver.get(site);
+				driver.manage().window().maximize();
+				break;
+
+			case "FIREFOX":
+				System.setProperty("webdriver.chorme.driver", "./Driver/chromedriver.exe");
+				driver = new FirefoxDriver();
+				driver.get(site);
+				driver.manage().window().maximize();
+				break;
+
+			}
+
+		} else {
+			System.out.println("Navegador não suprimido, digite CHROME e FIREFOX ");
+
 		}
-		
-	
-		
-	} else {
-	System.out.println("Navegador não suprimido, digite CHROME e FIREFOX ");
-		
-			
-		
+
 	}
-	
+
+//Preencher
+
+	public void preencher(By element, String text) {
+		driver.findElement(element).sendKeys(text);
+
+	}
+
+	// Clicar
+
+	public void submit(By element) {
+		driver.findElement(element).submit();
+
+		// Clicar Metodo de teste
+
+	}
 }
-
-}
-	
-
-	
-	
-
-
-
-
-	
-	
-
